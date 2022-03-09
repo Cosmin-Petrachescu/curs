441429fcce22f056d503d0330137574d53a23f77
main <b00190588t@gmail.com>
2022-03-09 10:04:04 +0200

    rest

--- index.js
+++
@@ -1,9 +0,0 @@
-var p = document.getElementById("greenplanet")
-
-p.innerText = "An alien appeared"
-p.innerHTML = "<b>An alien appeared"
-
-// p.style.color = "gold"
-// p.style.backgroundColor = "green"
-
-p.className = "warning"
